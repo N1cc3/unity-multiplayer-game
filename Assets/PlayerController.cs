feac443533aa3d8fcaa9afd73971a1c35b063bb5
Niclas Lindgren <niclaslindgren91@hotmail.com>
2019-10-13 12:52:11 +0300

    Add terrain, turret, and player controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalSpeed = 1.0f;
+    public float verticalSpeed = 1.0f;
+
+    public GameObject horizontalTransform;
+    public GameObject verticalTransform;
+
+    private void Update()
+    {
+        var h = horizontalSpeed * Input.GetAxis("Mouse X");
+        var v = verticalSpeed * Input.GetAxis("Mouse Y");
+        horizontalTransform.transform.Rotate(Vector3.up, h);
+        verticalTransform.transform.Rotate(Vector3.right, v);
+    }
+}
