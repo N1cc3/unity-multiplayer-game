68100da8596b2e033dfe5296888bc9622b129208
Niclas Lindgren <niclaslindgren91@hotmail.com>
2019-10-13 13:58:58 +0300

    Add shooting

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using static UnityEngine.ForceMode;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,11 +13,31 @@ public class PlayerController : MonoBehaviour
     public GameObject horizontalTransform;
     public GameObject verticalTransform;
 
+    public GameObject shotType;
+    public GameObject muzzle;
+    public float fireRate = 0.1f;
+    public float muzzleImpulse = 0.5f;
+
+    private float _shotDelta = 0.0f;
+
     private void Update()
     {
         var h = horizontalSpeed * Input.GetAxis("Mouse X");
         var v = verticalSpeed * Input.GetAxis("Mouse Y");
         horizontalTransform.transform.Rotate(Vector3.up, h);
         verticalTransform.transform.Rotate(Vector3.right, v);
+
+        _shotDelta = Math.Min(fireRate, _shotDelta + Time.deltaTime);
+        var isShooting = Input.GetButton("Fire1");
+        if (isShooting && _shotDelta >= fireRate) Shoot();
+    }
+
+    private void Shoot()
+    {
+        var rotation = muzzle.transform.rotation;
+        var shot = Instantiate(shotType, muzzle.transform.position, rotation);
+        var shotDirection = rotation * Vector3.back;
+        shot.GetComponent<Rigidbody>().AddForce(muzzleImpulse * shotDirection, Impulse);
+        _shotDelta = 0.0f;
     }
 }
