7115c6ff09201ce589c0d97d3214315be1896be0
Niclas Lindgren <niclaslindgren91@hotmail.com>
2019-10-17 23:04:28 +0300

    Add shot trail, lifetime, and max ricochets

---
+++ Assets/Shot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Shot : MonoBehaviour {
+	public float lifetime;
+
+	private float _timeSpawned;
+	private int _ricochets;
+
+	private void Start() {
+		_timeSpawned = Time.time;
+		_ricochets = Random.value > 0.5 ? 1 : 0;
+	}
+
+	private void Update() {
+		if (Time.time - _timeSpawned > lifetime) Destroy(gameObject);
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		if (_ricochets-- <= 0) Destroy(gameObject);
+	}
+}
